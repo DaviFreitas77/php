3b9ae9e83fd5f76d523f0743fc11ec7bff6fbaa4
DaviFreitas77 <146572313+DaviFreitas77@users.noreply.github.com>
2024-09-17 00:00:10 -0300

    update

--- searchObjeto.php
+++ searchObjeto.php
@@ -11,18 +11,10 @@ $cor = $json['cor'] ?? null;
 $marca = $json['marca'] ?? null;
 
 
-$sql = 'SELECT idPost, objeto.idObjeto, idPost, nomeObjeto, categoriaObjeto, localidadeObjeto, images, nome, imagem, descObjeto, dataRegistro, descMarca,descTamanho,descAndar,descLocal,descCor,descSubCategoria,descCategoria
+$sql = 'SELECT idPost, objeto.idObjeto, idPost, nomeObjeto, categoriaObjeto, tamanhoObjeto, localidadeObjeto, andar, corObjeto, images, nome, imagem, descObjeto, dataRegistro, marcaObjeto 
 FROM post 
 INNER JOIN objeto ON post.idObjeto = objeto.idObjeto
-INNER JOIN users ON post.idUsuario = users.id
-INNER JOIN 
-    tbmarca ON objeto.marcaObjeto = tbmarca.idMarca
-INNER JOIN tbtamanho on objeto.tamanhoObjeto = tbtamanho.idTamanho
-INNER JOIN tbandar on objeto.andar = tbandar.idAndar
-INNER JOIN tblocal on objeto.localidadeObjeto = tblocal.idLocal
-INNER JOIN tbcor on objeto.corObjeto = tbcor.idCor
-INNER JOIN tbsubcategoria on objeto.nomeObjeto = tbsubcategoria.idSubCategoria
-inner JOIN tbcategoria on objeto.categoriaObjeto = tbcategoria.idCategoria
+INNER JOIN users ON post.idUsuario = users.id 
         WHERE 1=1'; 
 
 $params = [];
